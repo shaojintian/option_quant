37bce45ea65715c907414ce0427017019357f6d6
shaojintian <40530183+shaojintian@users.noreply.github.com>
2023-07-31 23:55:07 +0800

    1

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
+# 15天tqqq卖put
+from datetime import date
+from longbridge.openapi import TradeContext, Config,QuoteContext
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+config = Config.from_env()
+ctx = QuoteContext(config)
+account = TradeContext(config)
+###################
+SYMBOL = ["TQQQ.US","SOXL.US","SPXL.US"]
+EXPIRY = date(2023,8,15)
+STRIKE = 400
+OPTION_TYPE = "PUT"
+ORDER_TYPE = "LMT"
+
+PUTS_WAITED_TO_SELL = []
+
+# # 查询账户余额
+# resp = account.account_balance()
+# print(resp)
+
+#
+resp = ctx.trades(SYMBOL[0], 1)
+print(resp)
+
+resp = ctx.option_chain_info_by_date(SYMBOL[0],date(2023, 8, 18))
+print(resp)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# # 查询期权信息
+# resp = ctx.option_quote(PUTS_WAITED_TO_SELL)
+# print(resp)
